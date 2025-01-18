385f6981e72b749d63193b460a21c3bfe8216a32
tbui17 <86227911+tbui17@users.noreply.github.com>
2025-01-17 21:03:51 -0700

    suggested changes

--- src/mergeAll.ts
+++ src/mergeAll.ts
@@ -1,28 +1,29 @@
-import type { MergeTuple } from "./internal/types/MergeTuple";
+import type { Merge } from "type-fest";
+import type { IterableContainer } from "./internal/types/IterableContainer";
+
+/**
+ * Merges a tuple of objects types into a single object type from left to right.
+ */
+type MergeTuple<
+  T extends IterableContainer,
+  Result = object, // no-op for the first iteration in the successive merges, also infers object as type by default if an empty tuple is used
+> = T extends readonly [infer Head, ...infer Rest]
+  ? MergeTuple<Rest, Merge<Result, Head>>
+  : Result;
 
 /**
  * Merges a list of objects into a single object.
  *
- * @param array - The array of objects.
+ * @param objects - The array of objects.
  * @signature
  *    R.mergeAll(objects)
  * @example
  *    R.mergeAll([{ a: 1, b: 1 }, { b: 2, c: 3 }, { d: 10 }]) // => { a: 1, b: 2, c: 3, d: 10 }
  * @category Array
  */
-export function mergeAll<A>(array: readonly [A]): A;
-export function mergeAll<A, B>(
-  array: readonly [A, B],
-): MergeTuple<typeof array>;
-export function mergeAll<A, B, C>(
-  array: readonly [A, B, C],
-): MergeTuple<typeof array>;
-export function mergeAll<A, B, C, D>(
-  array: readonly [A, B, C, D],
-): MergeTuple<typeof array>;
-export function mergeAll<A, B, C, D, E>(
-  array: readonly [A, B, C, D, E],
-): MergeTuple<typeof array>;
+export function mergeAll<T extends IterableContainer>(
+  objects: T,
+): MergeTuple<T>;
 export function mergeAll(array: ReadonlyArray<object>): object;
 
 export function mergeAll(items: ReadonlyArray<object>): object {
